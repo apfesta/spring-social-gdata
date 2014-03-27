43ac2474bd804af7431603bc9661c2446aafebe3
apfesta <andy_festa@yahoo.com>
2014-03-27 10:33:49 -0400

    Add some basic spreadsheet methods

--- spring-social-gdata/src/main/java/org/springframework/social/gdata/api/SpreadsheetOperations.java
+++ spring-social-gdata/src/main/java/org/springframework/social/gdata/api/SpreadsheetOperations.java
@@ -3,7 +3,11 @@
  */
 package org.springframework.social.gdata.api;
 
+import java.util.List;
+
 import com.google.gdata.client.spreadsheet.SpreadsheetService;
+import com.google.gdata.data.spreadsheet.CellEntry;
+import com.google.gdata.data.spreadsheet.SpreadsheetEntry;
 
 /**
  * @author apfesta
@@ -11,6 +15,10 @@ import com.google.gdata.client.spreadsheet.SpreadsheetService;
  */
 public interface SpreadsheetOperations {
 	
-	public SpreadsheetService getSpreadsheetService(String applicationName);
+	public SpreadsheetService getSpreadsheetService();
+	
+	public List<SpreadsheetEntry> getSpreadsheets();
+	
+	public List<CellEntry> getCells(String cellFeedUrl);
 
 }
